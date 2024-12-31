8301382ce70af1316161bd17b33cd5c61fd66ed7
AsfaaKhan <khanzadiasfi0314@gmail.com>
2024-12-31 21:08:37 +0500

    Fixed Navbar

--- src/components/HeroPage.tsx
+++ src/components/HeroPage.tsx
@@ -14,9 +14,9 @@ const Font = Roboto_Mono({
 )
 export default function HeroPage() {
     return (
-        <div className="bg mt-10  ">
+        <div className="bg-[url('/images/bg.jpg')]  mt-10 bg-cover bg-center bg-no-repeat h-[300px] sm:h-[400px] md:h-[500px] lg:[800px] ">
             <div className="flex flex-col gap-8 justify-center items-center pt-[100px]">
-            <h1 className={`${Font.className}  text-4xl mt-20 sm:text-6xl md:text-8xl font-extrabold  text-blue-950 `}>
+            <h1 className={`${Font.className}  text-5xl sm:text-6xl md:text-8xl font-extrabold  text-blue-950 `}>
                 CLICK - WORTHY
             </h1>
             <Link  href={'/blogPage'} className={`${Font2.className} font-bold text-2xl sm:text-4xl md:text-6xl text-white hover:text-blue-950 hover:border-blue-950 p-3 rounded-full hover:ring-1 shadow-md hover:shadow-lg`}>

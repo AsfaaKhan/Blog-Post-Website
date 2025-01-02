aacf955acb4deb829fcf04122b425f118f51056a
Asfaa Khan <159608757+AsfaaKhan@users.noreply.github.com>
2025-01-03 00:20:10 +0500

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,15 +3,9 @@ import { Montserrat } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/Header";
 import Footer2 from "@/components/Footer";
-// import Container from "@/components/Container";
-// import Headline from "@/components/Headline";
 import Head from "./head";
-// app/layout.tsx or app/layout.js
 import 'bootstrap/dist/css/bootstrap.min.css';
-// import 'bootstrap/dist/js/bootstrap.bundle.min.js';
-
 import Script from "next/script";
-
 
 const montserrat = Montserrat({ subsets: ["latin"] });
 
@@ -26,8 +20,6 @@ export default function RootLayout({
       <Head/>
       <body
         className={`${montserrat.className} bg-light dark:bg-dark `}>
-        {/* <Headline/> */}
-         
           <Header />
           {children}
           <Script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" />
